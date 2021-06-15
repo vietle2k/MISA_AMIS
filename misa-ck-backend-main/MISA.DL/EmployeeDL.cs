e73ae0256440fa72a27b9b43bdded0e0af5b06fe
vietle2k <49347369+vietle2k@users.noreply.github.com>
2021-06-15 14:03:14 +0700

    main

---
+++ misa-ck-backend-main/MISA.DL/EmployeeDL.cs
@@ -0,0 +1,28 @@
+﻿using Common.Entities;
+using Dapper;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.DL
+{
+    public class EmployeeDL:BaseDL
+    {
+        public bool CheckEmployeeCodeExist(string employeeCode)
+        {
+            using (_dbConnection = new MySqlConnection(_connectionString))
+            {
+                // 3. Thực thi lệnh lấy dữ liệu trong Database:
+                var sqlCommand = $"Proc_CheckEmployeeCodeExist";
+                DynamicParameters dynamicParameters = new DynamicParameters();
+                dynamicParameters.Add("@EmployeeCode", employeeCode);
+                var res = _dbConnection.ExecuteScalar<bool>(sqlCommand, dynamicParameters, commandType: CommandType.StoredProcedure);
+                return res;
+            }
+        }
+    }
+}
---
+++ misa-ck-backend-main/MISA.DL/Interfaces/IBaseDL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.DL.Interfaces
+{
+    public interface IBaseDL
+    {
+        IEnumerable<MISAEntity> GetAll<MISAEntity>();
+        MISAEntity GetById<MISAEntity>(Guid id);
+        int Insert<MISAEntity>(MISAEntity entity);
+        int Update<MISAEntity>(MISAEntity entity, Guid id);
+        int Delete<MISAEntity>(Guid id);
+    }
+}
